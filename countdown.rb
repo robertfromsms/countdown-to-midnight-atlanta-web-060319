86e65b99d27bedf52d5aab397ef1a738cd1e92e7
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-14 21:41:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
-
-def countdown
+def countdown(start_num)
+  counter = start_num
+  while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    x -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
